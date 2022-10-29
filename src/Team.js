7592164736ef3f38beaa5aa88e2fa4a9525cb10f
Valeriy Yakhin <VYakhin-consultant@Scholastic.com>
2022-10-29 16:21:43 +0400

    Team cards styles

--- src/Team.js
+++ src/Team.js
@@ -1,14 +1,33 @@
 import './Team.scss';
+import Avatar from 'boring-avatars';
 
 function Team(props) {
     return (
         <div className={props.alive ? 'team' : 'team died'}>
-            <span className="team-id text">{props.teamId}</span>
-            <span className="team-name text">{props.name}</span>
-            <div className="stats">
-                <span className="stren text">{props.strength}</span>
-                <span className="accur text">{props.accuracy*100}%</span>
+            <div className="top-part">
+                <div className="avatar">
+                    <Avatar
+                        size={75}
+                        name={props.name}
+                        variant="beam"
+                    />
+                </div>
+                <span className='team-name text'>{props.name}</span>
             </div>
+            <div className="bottom-part">
+                <span className='team-id text'>{props.teamId}</span>
+                <div className='stats'>
+                    <span className='stren text stat-item'>
+                        <span className="label">Сила</span>
+                        <span className="value">{props.strength}</span>
+                    </span>
+                    <span className='accur text stat-item'>
+                        <span className="label">Точность</span>
+                        <span className="value">{props.accuracy*100}%</span>
+                    </span>
+                </div>
+            </div>
+
         </div>
     );
 }
